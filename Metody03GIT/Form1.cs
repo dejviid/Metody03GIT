2fc190fae96767ae301ab9b1117c0a6018a15282
dejviid <davidmilostny@seznam.cz>
2021-03-08 12:13:46 +0100

    Přidejte soubory projektu.

---
+++ Metody03GIT/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metody03GIT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double vzdalenost(double a1, double a2, double b1, double b2)
+        {
+            return Math.Sqrt((a1 - b1) * (a1 - b1) + (a2 - b2) * (a2 - b2));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a1 = Convert.ToInt32(textBox1.Text), a2 = Convert.ToInt32(textBox3.Text), b1 = Convert.ToInt32(textBox3.Text), b2 = Convert.ToInt32(textBox3.Text);
+            MessageBox.Show("Vzdalenost je: " + vzdalenost(a1, a2, b1, b2).ToString());
+        }
+    }
+}
